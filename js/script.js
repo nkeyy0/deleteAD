482ec4ea825742bbd4ae4a7e4da3e22a2731414a
Nikita <komkovnk7@gmail.com>
2020-07-11 19:12:05 +0300

    fix errors

--- js/script.js
+++ js/script.js
@@ -14,22 +14,7 @@ P.S. Здесь есть несколько вариантов решения з
 "Добавляем любимый фильм"
 
 5) Фильмы должны быть отсортированы по алфавиту *//* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
+*/
 'use strict';
 
 const movieDB = {
@@ -41,13 +26,7 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-//1
-const promoADV = document.querySelectorAll('.promo__adv img');
 
-
-promoADV.forEach(item =>{
-    item.remove();
-});
 
 //2
 
@@ -56,10 +35,6 @@ promoGenre.textContent = 'Драма';
 
 //3
 
-const promoBG = document.querySelector('.promo__bg');
-
-promoBG.style.backgroundImage = "url('img/bg.jpg')";
-//console.log(promoBG);
 
 
 
@@ -69,24 +44,38 @@ promoBG.style.backgroundImage = "url('img/bg.jpg')";
 Для получения доступа к значению input - обращаемся к нему как input.value;
 P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.*/
 
+//1
+
 const conf = document.querySelector('.add'),
       lastFilm = document.querySelector('.adding__input'),
       check = conf.querySelector('[type = "checkbox"]'),
+      promoADV = document.querySelectorAll('.promo__adv img'),
+      promoBG = document.querySelector('.promo__bg'),
       movieList = document.querySelector('.promo__interactive-list');
+
+
+      
+
 
 
 let sortFilms = movieDB.movies.sort();
     
-createMovieList();
+createMovieList(movieDB.movies, movieList);
+removeAD();
+changeBackground('img/bg.jpg');
 
  function addFilm(event){
     event.preventDefault();
+    if(lastFilm.value){
     movieDB.movies.push(lastFilm.value);
     sortFilms = movieDB.movies.sort(compareFunction);
-    createMovieList();
+    createMovieList(movieDB.movies, movieList);
     if(check.checked == true){
         console.log("Добавляем любимый фильм");
     }
+    }
+    
+    event.target.reset();
  }
 
  function compareFunction(a , b){
@@ -99,16 +88,34 @@ createMovieList();
          return 0;
         }
 
- function createMovieList(){
-    movieList.innerHTML = "";   
-    movieDB.movies.forEach(function(item, i){
-        if(movieDB.movies[i].length > 21){
-        sortFilms[i] = `${movieDB.movies[i].slice(0, 21)}...`;
+ function createMovieList(films, parent){
+    parent.innerHTML = "";   
+    films.forEach(function(item, i){
+        if(films[i].length > 21){
+        sortFilms[i] = `${films[i].slice(0, 21)}...`;
         }
     movieList.innerHTML +=`<li class="promo__interactive-item">${i+1} ${sortFilms[i]} 
     <div class="delete"></div>
     </li>`;
     });
+    document.querySelectorAll('.delete').forEach(function(del, i){
+        del.addEventListener('click', function(){
+            del.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+            createMovieList(films, parent);
+        });
+
+ });
+}
+
+ function removeAD(){
+    promoADV.forEach(item =>{
+    item.remove();
+    });
+ }
+
+ function changeBackground(url){
+    promoBG.style.backgroundImage = `url(${url})`;
  }
  
  conf.addEventListener('submit', addFilm);
